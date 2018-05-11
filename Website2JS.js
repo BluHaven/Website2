cc8159289ea0e095c9754e90315422942f6c3bd2
Aisha <marianicole78@gmail.com>
2018-05-11 13:02:24 -0400

    Update Website2JS.js

--- Website2JS.js
+++ Website2JS.js
@@ -1,6 +1,6 @@
-<script>
-      
-  function myfunction() {
-    document.getElementById("emailsubmitbutton").value= "Submitted!";
-}
-</script>
+function myfunction() {
+	 document.getElementById("emailsubmitbutton").value= "Submitted!";
+	  alert("Email address has been added to database");
+    }
+    
+      var uponLoad = alert("Enter If You Dare");
